f2036c9f3669ee151e936cafe118d1584327661e
Scott Borecki <scottborecki@gmail.com>
2021-07-15 19:53:06 -0600

    Fix #26: Add pet names and links to view

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,27 +3,55 @@ require 'rails_helper'
 RSpec.describe ' applications show' do
   describe 'as a visitor' do
     describe 'when I visit an applications show page' do
-      it 'can see the applications attributes' do
-        applicant = Application.create!(
-          name: 'Scott',
-          street_address: '123 Main Street',
-          city: 'Denver',
-          state: 'Colorado',
-          zip_code: '80202',
-          description: 'Great with animals!',
-          status: 'pending'
-        )
+      before :each do
+        @shelter = Shelter.create!(name: 'Aurora shelter',
+                                   city: 'Aurora, CO',
+                                   foster_program: false,
+                                   rank: 9)
 
-        visit "/applications/#{applicant.id}"
+        @pet_1 = Pet.create!(adoptable: true,
+                             age: 1,
+                             breed: 'sphynx',
+                             name: 'Lucille Bald',
+                             shelter_id: @shelter.id)
 
-        expect(page).to have_content(applicant.name)
-        expect(page).to have_content(applicant.street_address)
-        expect(page).to have_content(applicant.city)
-        expect(page).to have_content(applicant.state)
-        expect(page).to have_content(applicant.zip_code)
-        expect(page).to have_content(applicant.description)
-        expect(page).to have_content(applicant.status)
-        # Add tests for name of all pets with links to pets show pages
+        @pet_2 = Pet.create!(adoptable: true,
+                             age: 3,
+                             breed: 'doberman',
+                             name: 'Lobster',
+                             shelter_id: @shelter.id)
+
+        @scott = Application.create!(name: 'Scott',
+                                     street_address: '123 Main Street',
+                                     city: 'Denver',
+                                     state: 'Colorado',
+                                     zip_code: '80202',
+                                     description: 'Great with animals!',
+                                     status: 'pending')
+        @scott.pets << @pet_1
+        @scott.pets << @pet_2
+      end
+
+      it 'can show the applications attributes' do
+        visit "/applications/#{@scott.id}"
+
+        expect(page).to have_content(@scott.name)
+        expect(page).to have_content(@scott.street_address)
+        expect(page).to have_content(@scott.city)
+        expect(page).to have_content(@scott.state)
+        expect(page).to have_content(@scott.zip_code)
+        expect(page).to have_content(@scott.description)
+        expect(page).to have_link(@pet_1.name)
+        expect(page).to have_link(@pet_2.name)
+        expect(page).to have_content(@scott.status)
+      end
+
+      it 'can link to the pet pages' do
+        @scott.pets.each do |pet|
+          visit "/applications/#{@scott.id}"
+          click_on "#{pet.name}"
+          expect(current_path).to eq("/pets/#{pet.id}")
+        end
       end
     end
   end
