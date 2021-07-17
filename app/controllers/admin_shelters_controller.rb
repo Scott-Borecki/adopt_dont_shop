6ca1a8ba35d590baf909918d9094630fa6743f70
Scott Borecki <scottborecki@gmail.com>
2021-07-17 17:30:09 -0600

    Add Shelters with pending applications to view (#16)

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -1,5 +1,6 @@
 class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.order_by_name_reverse
+    @shelters_with_pending_applications = Shelter.with_pending_applications
   end
 end
