369cac0f10a5ab9b63456e05c34bbfa026cbade4
Scott Borecki <scottborecki@gmail.com>
2021-07-18 17:58:08 -0600

    Add action required view (#2)

--- spec/features/admin/shelter/show_spec.rb
+++ spec/features/admin/shelter/show_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'the admin shelter show' do
   before :each do
+    # SHELTERS
     @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create!(name: 'Littleton shelter', city: 'Littleton, CO', foster_program: true, rank: 7)
     @shelter_3 = Shelter.create!(name: 'Denver shelter', city: 'Denver, CO', foster_program: true, rank: 10)
     @shelter_4 = Shelter.create!(name: 'Centennial shelter', city: 'Cenntenial, CO', foster_program: false, rank: 4)
 
+    # PETS - SHELTER 1
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
     @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
     @pet_3 = Pet.create!(adoptable: true, age: 8, breed: 'spanial', name: 'Bear', shelter_id: @shelter_1.id)
 
+    # PETS - SHELTER 2
     @pet_4 = Pet.create!(adoptable: true, age: 2, breed: 'hound', name: 'Dolly', shelter_id: @shelter_2.id)
     @pet_5 = Pet.create!(adoptable: true, age: 4, breed: 'lab', name: 'Yeller', shelter_id: @shelter_2.id)
 
+    # PETS - SHELTER 3
     @pet_6 = Pet.create!(adoptable: true, age: 1, breed: 'pig', name: 'Babe', shelter_id: @shelter_3.id)
     @pet_7 = Pet.create!(adoptable: true, age: 2, breed: 'orange tabby', name: 'Milo', shelter_id: @shelter_3.id)
   end
@@ -87,6 +91,62 @@ RSpec.describe 'the admin shelter show' do
           within '#statistics' do
             expect(page).to have_content("Number of Pets Adopted: #{shelter.number_of_pets_adopted}")
           end
+        end
+      end
+    end
+
+    describe 'Then I see a section title "Action Required"' do
+      before :each do
+        # PETS - SHELTER 3 (continued)
+        @pet_8 = Pet.create!(adoptable: true, age: 2, breed: 'pug', name: 'Otis', shelter_id: @shelter_3.id)
+
+        # PETS - SHELTER 4
+        @pet_9 = Pet.create!(adoptable: true, age: 2, breed: 'st. bernard', name: 'Beethoven', shelter_id: @shelter_4.id)
+        @pet_10 = Pet.create!(adoptable: true, age: 7, breed: 'bulldog', name: 'Chance', shelter_id: @shelter_4.id)
+        @pet_11 = Pet.create!(adoptable: true, age: 6, breed: 'golden retriever', name: 'Shadow', shelter_id: @shelter_4.id)
+        @pet_12 = Pet.create!(adoptable: true, age: 8, breed: 'himalayan cat', name: 'Sassy', shelter_id: @shelter_4.id)
+
+        # APPLICATIONS
+        @bob = Application.create!(name: 'Bob', street_address: '456 Main Street', city: 'Denver', state: 'Colorado', zip_code: '80202', description: 'Great with animals!', status: 'In Progress')
+        @scott = Application.create!(name: 'Scott', street_address: '123 Main Street', city: 'Denver', state: 'Colorado', zip_code: '80202', description: 'Great with animals!', status: 'Pending')
+        @sierra = Application.create!(name: 'Sierra', street_address: '789 Main Street', city: 'Arvada', state: 'Colorado', zip_code: '80003', description: 'Great with animals!', status: 'Accepted')
+        @laura = Application.create!(name: 'Laura', street_address: '1550 Main Street', city: 'Aurora', state: 'Colorado', zip_code: '80010', description: 'Great with animals!', status: 'Rejected')
+
+        # APPLICATION PETS - BOB
+        @bob.pets << @pet_3
+        @bob.pets << @pet_6
+        @bob.pets << @pet_7
+
+        # APPLICATION PETS - SCOTT
+        @scott.pets << @pet_1
+        @scott.pets << @pet_2
+        @scott.pets << @pet_4
+
+        # APPLICATION PETS - SIERRA
+        @sierra.pets << @pet_5
+        @sierra.pets << @pet_8
+        @sierra.pets << @pet_9
+
+        @pet_5.update(adoptable: false)
+        @pet_8.update(adoptable: false)
+        @pet_9.update(adoptable: false)
+
+        # APPLICATION PETS - LAURA
+        @laura.pets << @pet_10
+        @laura.pets << @pet_11
+        @laura.pets << @pet_12
+      end
+
+      it 'lists all the pets for the shelter with pending applications' do
+        visit visit "/admin/shelters/#{@shelter_1.id}"
+
+        expect(page).to have_content('Action Required')
+        save_and_open_page
+        
+        within '#action-required' do
+          expect(page).to have_content(@pet_1.name)
+          expect(page).to have_content(@pet_2.name)
+          expect(page).to_not have_content(@pet_3.name)
         end
       end
     end
