0d4895d0ca4ce70e5c1a605ef52ddbeda7e7179e
Scott Borecki <scottborecki@gmail.com>
2021-07-20 07:27:52 -0600

    Add #actions_required

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -50,5 +50,41 @@ RSpec.describe Pet, type: :model do
         expect(@pet_1.adoptable).to be(false)
       end
     end
+
+    describe '.actions_required' do
+      it 'returns the applications where action is required for pet'  do
+        scott = Application.create!( name: 'Scott', street_address: '123 Main Street', city: 'Denver', state: 'Colorado', zip_code: '80202', description: 'Great with animals!', status: 'Pending')
+        bob = Application.create!( name: 'Bob', street_address: '456 Main Street', city: 'Denver', state: 'Colorado', zip_code: '80202', description: 'Great with animals!', status: 'Pending')
+        sierra = Application.create!(name: 'Sierra', street_address: '789 Main Street', city: 'Arvada', state: 'Colorado', zip_code: '80003', description: 'Great with animals!', status: 'Accepted')
+        laura = Application.create!(name: 'Laura', street_address: '1550 Main Street', city: 'Aurora', state: 'Colorado', zip_code: '80010', description: 'Great with animals!', status: 'Rejected')
+
+        scott.pets << @pet_1
+        scott.pets << @pet_2
+        bob.pets << @pet_1
+        bob.pets << @pet_2
+        sierra.pets << @pet_3
+        laura.pets << @pet_1
+        laura.pets << @pet_2
+
+        application_pet_1_scott = @pet_1.application_pets.find_by(application_id: scott.id)
+        application_pet_2_scott = @pet_2.application_pets.find_by(application_id: scott.id)
+
+        application_pet_1_bob = @pet_1.application_pets.find_by(application_id: bob.id)
+        application_pet_2_bob = @pet_2.application_pets.find_by(application_id: bob.id)
+        application_pet_1_bob.update(status: 'Approved')
+
+        application_pet_3_sierra = @pet_3.application_pets.find_by(application_id: sierra.id)
+        application_pet_3_sierra.update(status: 'Approved')
+
+        application_pet_1_laura = @pet_1.application_pets.find_by(application_id: laura.id)
+        application_pet_2_laura = @pet_2.application_pets.find_by(application_id: laura.id)
+        application_pet_1_laura.update(status: 'Rejected')
+        application_pet_2_laura.update(status: 'Approved')
+
+        expect(@pet_1.actions_required).to eq([scott])
+        expect(@pet_2.actions_required).to eq([scott, bob])
+        expect(@pet_3.actions_required).to eq([])
+      end
+    end
   end
 end
